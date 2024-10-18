695f487bc89574bd1eafa6bce6c89c647f1bcd8c
moon <ans92175@gmail.com>
2024-10-18 20:29:16 +0900

    add refer

--- conv_test.py
+++ conv_test.py
@@ -1,3 +1,14 @@
+'''
+NetVLAD: Relja Arandjelović, et al, NetVLAD: CNN architecture for weakly supervised place recognition, CVPR, 2016.
+PatchNetVLAD:Stephen Hausler, et al, Patch-NetVLAD: Multi-Scale Fusion of Locally-Global Descriptors for Place Recognition, CVPR, 2021.
+-> two stage, e.g. TransVPR, SuperGlue
+cosplace: Gabriele Berton et al. Rethinking Visual Geo-localization for Large-Scale Applications, CVPR, 2022.
+mixvpr: Amar Ali-bey, et al, MixVPR: Feature Mixing for Visual Place Recognition, WACV, 2023.
+GeM: Filip Radenovic, et al, Finetuning CNN image retrieval with no human annotation, TPAMI, 2018,
+convAP: Amar Ali-bey, et al, GSV-Cities: Toward Appropriate Supervised Visual Place Recognition, Neurocomputing, 2022.
+'''
+
+
 import os
 import sys
 import configparser
