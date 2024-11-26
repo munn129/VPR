9c4a63a38aa7d190a557f08c57b81a8667fe6d5c
moon <ans92175@gmail.com>
2024-11-26 22:57:15 +0900

    edit shell

--- dim_ex.sh
+++ dim_ex.sh
@@ -12,13 +12,12 @@ do
   do
     for dim in $DIMS
     do
+      VERSION="${imagesize}_${image}_${dim}_${POSTFIX}"
+      echo "=================================================="
+      echo "${METHOD} ${VERSION} ${date} is start at $(date +%H):$(date +%M):$(date +%S)"
+      
       for date in $DATES
       do
-
-        VERSION="${imagesize}_${image}_${dim}_${POSTFIX}"
-        echo "=================================================="
-        echo "${METHOD} ${VERSION} ${date} is start at $(date +%H):$(date +%M):$(date +%S)"
-
         python feature_extract.py \
           --method $METHOD \
           --dataset_dir /media/moon/moon_ssd/moon_ubuntu/post_oxford/$date/$image \
@@ -28,22 +27,25 @@ do
           --image_size $imagesize 
 
       done
+
+        echo "$VERSION feature matching start at $(date +%H):$(date +%M):$(date +%S)"
+        python feature_match.py \
+          --version $VERSION \
+          --method $METHOD \
+          --save_dir dim_ex_result
+
+
+        echo "$VERSION eval start at $(date +%H):$(date +%M):$(date +%S)"
+        python eval/eval.py \
+          --version $VERSION \
+          --method $METHOD
+
+
+        echo "${VERSION} is done at $(date +%H):$(date +%M):$(date +%S)"
+
     done
   done
 done
 
-
-echo "$VERSION feature matching start at $(date +%H):$(date +%M):$(date +%S)"
-python feature_match.py \
-  --version $VERSION \
-  --method $METHOD \
-  --save_dir dim_ex_result
-
-
-echo "$VERSION eval start at $(date +%H):$(date +%M):$(date +%S)"
-python eval/eval.py \
-  --version $VERSION \
-  --method $METHOD
-
-
-echo "done at $(date +%H):$(date +%M):$(date +%S)"
+echo "=================================================="
+echo "Done at $(date +%H):$(date +%M):$(date +%S)"
